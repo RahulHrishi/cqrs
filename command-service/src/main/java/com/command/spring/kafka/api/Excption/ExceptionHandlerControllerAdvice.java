eb0ac35388f0e3bbbf72429c58176671d3441c7f
SnoopSunny <sahasantanu8519@gmail.com>
2022-06-03 21:33:59 +0530

    Resolve conflict

--- command-service/src/main/java/com/command/spring/kafka/api/Excption/ExceptionHandlerControllerAdvice.java
+++ command-service/src/main/java/com/command/spring/kafka/api/Excption/ExceptionHandlerControllerAdvice.java
@@ -5,10 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ValidationException;
+
 @ControllerAdvice
 public class ExceptionHandlerControllerAdvice {
+
     @ExceptionHandler(value = AuctionException.class)
     public ResponseEntity<Object> exception(AuctionException exception) {
+        return new ResponseEntity<>("custom exception message", HttpStatus.BAD_GATEWAY);
+    }
+
+    @ExceptionHandler(value = ValidationException.class)
+    public ResponseEntity<Object> exception(ValidationException exception) {
         return new ResponseEntity<>("custom exception message", HttpStatus.BAD_GATEWAY);
     }
 }
